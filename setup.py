7c6b414f0b265fe12c22ddb9cdbeb3b29ed084b6
Casey Link <unnamedrambler@gmail.com>
2013-04-04 20:43:28 +0200

    setup wander app w/ 2.7 support

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='WanderOpenshift',
       version='1.0',
       description='OpenShift App',
       author='Your Name',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask>=0.7.2'],
+      install_requires=['Flask>=0.8','wander'],
+      dependency_links=['https://github.com/Ramblurr/wander/tarball/master#egg=wander']
      )
